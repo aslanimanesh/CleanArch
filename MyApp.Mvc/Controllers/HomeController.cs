e163504df7d93d28d6defa283adf46f13d4962a5
aslanimanesh <h.aslanimanesh@gmail.com>
2024-09-24 16:28:14 +0330

    Show Product In HomaPage

--- MyApp.Mvc/Controllers/HomeController.cs
+++ MyApp.Mvc/Controllers/HomeController.cs
@@ -1,32 +1,22 @@
 using Microsoft.AspNetCore.Mvc;
-using MyApp.Mvc.Models;
-using System.Diagnostics;
+using MyApp.Application.Interfaces;
 
 namespace MyApp.Mvc.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly IProductService _productService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(IProductService productService )
         {
-            _logger = logger;
+            _productService = productService;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var products = await _productService.GetAllAsync();
+            return View(products);
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
